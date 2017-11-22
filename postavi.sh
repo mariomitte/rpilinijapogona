d390b7019917028b470fa81f2d7057a00eb0de2a
mariomitte <mario_mitte@hotmail.com>
2017-11-22 14:15:51 +0100

    Update postavi.sh

--- postavi.sh
+++ postavi.sh
@@ -13,18 +13,27 @@ echo "Preuzmi nadogradnje\n"
 sudo apt-get -y upgrade
 
 echo "\n"
-echo "Instalacija Python biblioteka\n"
-sudo apt-get install -y build-essential python3-dev
+echo "Preuzmi git\n"
+sudo apt-get -y install git
+
+echo "\n"
+echo "Preuzmi projekt linijapogona\n"
+cd /home/pi/
+git clone https://github.com/mariomitte/rpilinijapogona.git
+
+echo "\n"
+echo "Preuzmi Python biblioteke\n"
+sudo apt-get install -y build-essential
+sudo apt-get install -y python3-dev
 sudo apt-get install -y python3-smbus
 sudo apt-get install -y python3-pip
 sudo apt-get install -y openssh-server
 sudo apt-get install -y python3-rpi.gpio
 sudo apt-get install -y python3-picamera
 
-echo "sada Django\n"
-sudo pip3 install virtualenv
 echo "\n"
-echo "Instaliraj potrebne biblioteke za rad sa Django-om u venv"
+echo "Instaliraj potrebne biblioteke za rad sa Django-om"
+cd linijapogona
 sudo pip3 install -r requirements.txt
 
 echo "\n"
