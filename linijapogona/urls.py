2de7bce49024feb816a6ac4f8e648fa3c2c49644
mariomitte <mario_mitte@hotmail.com>
2017-11-23 16:19:06 +0100

    Update urls.py

--- linijapogona/urls.py
+++ linijapogona/urls.py
@@ -17,8 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    
+    # /welcome/
+    url(r'^$', TemplateView.as_view(template_name='welcome.html'), name='welcome'),
+    
     # /admin/
     url(r'^admin/', admin.site.urls),
 
