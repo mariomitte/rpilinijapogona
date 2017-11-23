bb935f5776b51348fdee8fe42cc5db6f5b9f72b5
mariomitte <mario_mitte@hotmail.com>
2017-11-23 15:37:57 +0100

    Update urls.py

--- pogon1/api/urls.py
+++ pogon1/api/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from pogon1.api.views import CvorViewSet
+from upravljanje.api.views import UpravljanjeViewSet, CvorViewSet
 
 router = DefaultRouter()
-router.register('cvor', CvorViewSet)
+router.register(prefix='upravljanje', viewset=UpravljanjeViewSet)
 
 urlpatterns = [
-    #
+    url(r'^cvor/$', CvorViewSet.as_view()),
 ]
 
 urlpatterns += router.urls
